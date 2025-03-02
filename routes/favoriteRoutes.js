6da7ce64e88f045358ebd84d01fb71756ea184c2
teogarbarino <70756721+teogarbarino@users.noreply.github.com>
2025-03-02 17:44:40 +0100

    :construction: missing import

--- routes/favoriteRoutes.js
+++ routes/favoriteRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Item = require('../models/items');
 const Favorite = require('../models/favoris');
 const Item = require('../models/Item');
 const authMiddleware = require('../middleware/authMiddleware');
