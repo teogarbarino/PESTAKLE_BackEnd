fbf3826b5c7acb3fb8717ff0f3b41f580a8d5a1e
teogarbarino <70756721+teogarbarino@users.noreply.github.com>
2025-03-02 19:09:16 +0100

    Update itemRoutes.js

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Item = require('../models/items');
 const mongoose = require('mongoose');
-const authMiddleware = require('../middleware/authMiddleware');
+const { authMiddleware, adminMiddleware } = require('../middleware/authMiddleware');
 const itemOwnershipMiddleware = require('../middleware/itemMiddleware');
 const Report = require('../models/reports');
 
